fe379eab33435343b8316b9b79252590a1f4718d
Federico Di Petta <federico.dipetta@studenti.unicam.it>
2026-05-21 11:24:50 +0200

    forzo locale a US

--- src/main/java/it/unicam/cs/bdslab/tarnas/App.java
+++ src/main/java/it/unicam/cs/bdslab/tarnas/App.java
@@ -1,7 +1,9 @@
 package it.unicam.cs.bdslab.tarnas;
+import java.util.Locale;
 
 public class App {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Main.main(args);
     }
 }
